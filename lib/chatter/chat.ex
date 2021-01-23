bd8ed123e26c51aa0a28efda5c630e9e607b3f96
Owen Bickford <type1fool@users.noreply.github.com>
2021-01-23 18:44:25 -0500

    creating_rooms (#4)

--- lib/chatter/chat.ex
+++ lib/chatter/chat.ex
@@ -5,4 +5,20 @@ defmodule Chatter.Chat do
     Chat.Room
     |> Repo.all()
   end
+
+  def new_chat_room do
+    %Chat.Room{}
+    |> Chat.Room.changeset(%{})
+  end
+
+  def create_chat_room(params) do
+    %Chat.Room{}
+    |> Chat.Room.changeset(params)
+    |> Repo.insert()
+  end
+
+  def find_room(id) do
+    Chat.Room
+    |> Repo.get!(id)
+  end
 end

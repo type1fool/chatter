3e5ddc1daca41c68ff4769d14105348fe035f2bc
Owen Bickford <historicstork@gmail.com>
2021-01-23 17:28:46 -0500

    add ex_machina

--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Chatter.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:wallaby, "~> 0.26", [runtime: false, only: :test]}
+      {:wallaby, "~> 0.26", [runtime: false, only: :test]},
+      {:ex_machina, "~> 2.3", only: :test}
     ]
   end
 

8d1ec89ec35be3d47de3216eaf364e30e53d7d97
Owen Bickford <historicstork@gmail.com>
2021-01-23 20:23:34 -0500

    move chat logic to new file

---
+++ assets/js/chat_room.js
@@ -0,0 +1,30 @@
+import socket from "./socket"
+
+let chatRoomTitle = document.getElementById("chat-room-title")
+if (chatRoomTitle) {
+  let chatRoomName = chatRoomTitle.dataset.chatRoomName
+  let channel = socket.channel(`chat_room:${chatRoomName}`, {})
+
+  let messageForm = document.getElementById("new-message-form")
+  let messageInput = document.getElementById("message")
+  let messagesContainer = document.querySelector("[data-role='messages']")
+
+  messageForm.addEventListener("submit", event => {
+    event.preventDefault()
+    channel.push("new_message", { body: messageInput.value.trim() })
+    event.target.reset()
+  })
+
+  channel.on("new_message", payload => {
+    appendMessage(messagesContainer, payload.body)
+  })
+
+  function appendMessage(container, message) {
+    let messageItem = document.createElement("li")
+    messageItem.dataset.role = "message"
+    messageItem.innerText = message
+    container.appendChild(messageItem)
+  }
+
+  channel.join()
+}
--- assets/js/socket.js
+++ assets/js/socket.js
@@ -54,30 +54,4 @@ let socket = new Socket("/socket", { params: {} })
 // Finally, connect to the socket:
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let chatRoomTitle = document.getElementById("title")
-if (chatRoomTitle) {
-  let chatRoomName = chatRoomTitle.dataset.chatRoomName
-  let channel = socket.channel(`chat_room:${chatRoomName}`, {})
-
-  let form = document.getElementById("new-message-form")
-  let messageInput = document.getElementById("message")
-  let messages = document.querySelector("[data-role='messages']")
-
-  form.addEventListener("submit", event => {
-    event.preventDefault()
-    channel.push("new_message", { body: messageInput.value.trim() })
-    event.target.reset()
-  })
-
-  channel.on("new_message", payload => {
-    let messageItem = document.createElement("li")
-    messageItem.dataset.role = "message"
-    messageItem.innerText = payload.body
-    messages.appendChild(messageItem)
-  })
-
-  channel.join()
-}
-
 export default socket

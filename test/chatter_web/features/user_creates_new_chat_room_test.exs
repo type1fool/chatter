f3f3d58b591b447fb99fd5eb314ba4513ad737ca
Owen Bickford <historicstork@gmail.com>
2021-01-23 21:13:23 -0500

    add user to test

--- test/chatter_web/features/user_creates_new_chat_room_test.exs
+++ test/chatter_web/features/user_creates_new_chat_room_test.exs
@@ -2,16 +2,24 @@ defmodule ChatterWeb.UserCreatesChatRoomTest do
   use ChatterWeb.FeatureCase, async: true
 
   test "user creates a new chat room successfully", %{session: session} do
+    user = insert(:user)
+
     session
-    |> visit(rooms_index())
+    |> visit("/")
+    |> sign_in(as: user)
     |> click(new_chat_link())
     |> create_chat_room(name: "elixir")
     |> assert_has(room_title("elixir"))
   end
 
-  defp rooms_index, do: Routes.chat_room_path(@endpoint, :index)
-
   defp new_chat_link,do: Query.link("New chat room")
+
+  defp sign_in(session, as: user) do
+    session
+    |> fill_in(Query.text_field("Email"), with: user.email)
+    |> fill_in(Query.text_field("Password"), with: user.password)
+    |> click(Query.button("Sign In"))
+  end
 
   defp create_chat_room(session, name: name) do
     session

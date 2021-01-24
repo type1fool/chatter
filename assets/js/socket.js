41a55d6b851203648c4d954db4f77eaa8dca1b82
Owen Bickford <historicstork@gmail.com>
2021-01-23 20:17:05 -0500

    remove extra logging

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -78,8 +78,6 @@ if (chatRoomTitle) {
   })
 
   channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
 }
 
 export default socket

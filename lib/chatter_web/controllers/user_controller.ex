65662c6450ee2e282d4a30be92ff1c5ea976d515
Owen Bickford <historicstork@gmail.com>
2021-01-24 17:39:47 -0500

    alias doorman and repo, login user during create/2

--- lib/chatter_web/controllers/user_controller.ex
+++ lib/chatter_web/controllers/user_controller.ex
@@ -1,6 +1,8 @@
 defmodule ChatterWeb.UserController do
   use ChatterWeb, :controller
-  alias Chatter.User
+  alias Chatter.{Repo, User}
+  alias Doorman.Auth.Secret
+  alias Doorman.Login.Session
 
   def new(conn, _params) do
     changeset = User.changeset(%User{}, %{})
@@ -8,13 +10,14 @@ defmodule ChatterWeb.UserController do
   end
 
   def create(conn, %{"user" => params}) do
-    {:ok, _user} =
+    {:ok, user} =
       %User{}
       |> User.changeset(params)
       |> Secret.put_session_secret()
       |> Repo.insert()
 
     conn
+    |> Session.login(user)
     |> redirect(to: "/")
   end
 end

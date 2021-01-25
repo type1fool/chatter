e0f807a3396f9e3bc225721d1b10635b77872936
Owen Bickford <historicstork@gmail.com>
2021-01-24 20:38:43 -0500

    add postcss & tailwindcss, configure both, remove sass-loader

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -35,11 +35,11 @@ module.exports = (env, options) => {
           }
         },
         {
-          test: /\.[s]?css$/,
+          test: /\.(post)?css$/,
           use: [
             MiniCssExtractPlugin.loader,
             'css-loader',
-            'sass-loader',
+            'postcss-loader',
           ],
         }
       ]
@@ -48,6 +48,6 @@ module.exports = (env, options) => {
       new MiniCssExtractPlugin({ filename: '../css/app.css' }),
       new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
     ]
-    .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
+      .concat(devMode ? [new HardSourceWebpackPlugin()] : [])
   }
 };

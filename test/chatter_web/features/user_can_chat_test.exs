e2f9a3b7a6daeca418d8a8721dc4d4a4ebe18683
Owen Bickford <historicstork@gmail.com>
2021-01-24 19:33:06 -0500

    disable async

--- test/chatter_web/features/user_can_chat_test.exs
+++ test/chatter_web/features/user_can_chat_test.exs
@@ -1,5 +1,5 @@
 defmodule ChatterWeb.UserCanChatTest do
-  use ChatterWeb.FeatureCase, async: true
+  use ChatterWeb.FeatureCase, async: false
 
   test "user can chat with others successfully", %{metadata: metadata} do
     room = insert(:chat_room)
